f3d6b2538817fd193a2d2e0588d6025834037813
backwardslashn <backwardslashn@gmail.com>
2025-04-22 03:13:05 -0700

    Update tree.c

--- tree.c
+++ tree.c
@@ -2,27 +2,31 @@
 
 int main(int argc, char *argv[])
 {
-    int i = 0;
-    int j = 0;
-    printf("Half a tree");
+    int i;
+    printf("\n\t\t|--Number tree--|");
     
-    printf("Enter an number: ");
+    printf("\n\t\tEnter a number: ");
     scanf("%d", &i);
-    
-    printf("Enter an number: ");
-    scanf("%d", &j);
-    
+
     while (i < 11)
     {
-        j = i - 1;
-        printf("\n%d ", i);
-        i++;
+        for (int s = 0; s < 11 - i; s++)
+        {
+            printf("  ");
+        }
 
-        while (j >= 0)
+        for (int j = i; j >= 0; j--)
         {
             printf("%d ", j);
-            j--;
         }
+
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+
+        printf("\n");
+        i++;
     }
     return 0;
 }
